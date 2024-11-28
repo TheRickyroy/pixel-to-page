e2b8fc8e1df3a2550019fd8c0fccd39551426786
Ricky Smithson <rickyroysmithson@gmail.com>
2024-11-28 23:02:38 +0000

    add: blog post_list and view

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views import generic
+from .models import Post
 
-# Create your views here.
-
-class HomePage(TemplateView):
-  """
-  Displays home page"
-  """
-  template_name = 'base.html'
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1)
+    template_name = "blog.html"
