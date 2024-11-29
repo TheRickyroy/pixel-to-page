00885da72d0b18656a04dedee10a66503726de10
Ricky Smithson <rickyroysmithson@gmail.com>
2024-11-29 10:56:48 +0000

    add: index.html for blog template and associated view detail

--- blog/admin.py
+++ blog/admin.py
@@ -5,11 +5,17 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
-    list_display = ('title', 'slug', 'status', 'featured')
+    list_display = ('author', 'title', 'slug', 'status', 'created_on', 'updated_on', 'featured')
     search_fields = ['title']
-    list_filter = ('status',)
+    list_filter = ('status', 'featured',)
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
 
+@admin.register(Comment)
+class PostAdmin(SummernoteModelAdmin):
 
-admin.site.register(Comment)
+    list_display = ('author', 'approved', 'created_on', 'updated_on')
+    search_fields = ['title']
+    list_filter = ('approved',)
+    summernote_fields = ('body',)
+
--- pixel_to_page/settings.py
+++ pixel_to_page/settings.py
@@ -18,7 +18,6 @@ if os.path.isfile("env.py"):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
@@ -28,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Automatic DEBUG status - Provided by John during coding coach
 # DEBUG = 'DEBUG' in os.environ
@@ -137,7 +136,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
